f115a06cfbe55599189b96bc24ef5aa464bae86f
Richard Marafioti <marafioti@MacBook-Air.local>
2024-10-30 09:57:50 -0500

    created member folder in auth with index.js for express routes

--- api/member/index.js
+++ api/member/index.js
@@ -2,56 +2,35 @@ const prisma = require("../../prisma");
 const router = require("express").Router();
 module.exports = router;
 
-// GET to check the logged-in owner
+// GET to check the logged-in member
 router.get("/me", async (req, res, next) => {
   console.log("Authenticated user:", res.locals.user);
 
   try {
     // Ensure the user is authenticated and available in res.locals.user
+    const { businessName, code } = req.body;
     if (!res.locals.user) {
       return next({
         status: 401,
         message: "You are not logged in or do not have access",
       });
     }
-
-    const { id } = res.locals.user;
-
-    // Fetch the owner's details along with their businesses
-    const ownerWithBusiness = await prisma.owner.findUnique({
-      where: { id },
-      select: {
-        id: true,
-        username: true,
-        ownerName: true,
-        ownerBusiness: true, // Include linked businesses
-      },
-    });
-
-    // If the owner is not found, send a 404 error
-    if (!ownerWithBusiness) {
-      return next({
-        status: 404,
-        message: `No owner found with ID ${id}`,
-      });
-    }
-
-    // Respond with the owner details, including any linked businesses
-    res.json(ownerWithBusiness);
+    res.json();
   } catch (error) {
     next(error); // Forward errors to the error-handling middleware
   }
 });
 
-// Post route to create a new business
-router.post("/business", async (req, res, next) => {
+// Post route to create member info
+// need to update schema for onboarding which asks to create email and phone contact info for each user.
+/*router.post("/memberinfo", async (req, res, next) => {
   console.log("Authenticated user:", res.locals.user);
 
   try {
     const { id: owner_id } = res.locals.user;
     const { businessName, code } = req.body;
 
-    const newBusiness = await prisma.business.create({
+    const newMemberInfo = await prisma.business.create({
       data: {
         businessName,
         code,
@@ -64,3 +43,4 @@ router.post("/business", async (req, res, next) => {
     next(e);
   }
 });
+*/
