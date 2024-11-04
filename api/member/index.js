7afccd26a5f109c0d4dffdc1bc70b011b7b47552
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-11-04 16:57:19 -0600

    add to member get request to access drops and services as well

--- api/member/index.js
+++ api/member/index.js
@@ -26,6 +26,11 @@ router.get("/", requireMemberRole, async (req, res, next) => {
       where: { id: member.id },
       include: {
         business: true,
+        drop: {
+          include: {
+            service: true,
+          },
+        },
       },
       // Include any related data if necessary, e.g., member-specific associations
     });
